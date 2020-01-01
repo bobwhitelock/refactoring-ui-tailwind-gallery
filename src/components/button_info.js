185c83b0533cd10eabc667240cb5c359f24a4944
Bob Whitelock <bob.whitelock1@gmail.com>
2020-01-01 21:28:50 +0000

    Add remaining buttons

--- src/components/button_info.js
+++ src/components/button_info.js
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const ButtonInfo = ({ description, additionalClasses }) => {
+const ButtonInfo = ({ children, description, additionalClasses }) => {
   const classes = `bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 ${additionalClasses}`
+  if (!children) {
+    children = "Primary"
+  }
 
   return (
     <div className="mt-4">
       <h3 className="text-lg">{description}</h3>
-      <button className={classes}>Primary</button>
+      <button className={classes}>{children}</button>
     </div>
   )
 }
