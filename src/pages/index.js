8014f535464c8674173d257d9a023e5168e2a38e
Bob Whitelock <bob.whitelock1@gmail.com>
2020-01-01 20:42:15 +0000

    Add some buttons with gradients
    Using Tailwind gradients plugin.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import ButtonInfo from "../components/button_info"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+
     <div className="m-8">
       <ButtonInfo description="Small rounded" additionalClasses="rounded" />
       <ButtonInfo description="Large rounded" additionalClasses="rounded-lg" />
@@ -21,7 +22,16 @@ const IndexPage = () => (
         description="With soft shadow"
         additionalClasses="rounded shadow-lg"
       />
+      <ButtonInfo
+        description="Vertical gradient"
+        additionalClasses="rounded bg-gradient-t-gray-600"
+      />
+      <ButtonInfo
+        description="Horizontal gradient"
+        additionalClasses="rounded bg-gradient-r-gray-600"
+      />
     </div>
+
     <Link to="/page-2/" className="p-8 m-8 py-8">
       Go to page 2
     </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    linearGradients: theme => ({
+      colors: theme("colors"),
+    }),
+    radialGradients: theme => ({
+      colors: theme("colors"),
+    }),
+    conicGradients: theme => ({
+      colors: theme("colors"),
+    }),
   },
   variants: {},
-  plugins: []
+  plugins: [require("tailwindcss-gradients")()],
 }
